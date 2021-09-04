630fb605f80f28adbff8f5bd84bf08f9332e0ea7
MarcusButt <MarcusButt@users.noreply.github.com>
2021-09-04 12:07:09 -0230

    Add Variables for 52 week high and low

--- rs_screener.py
+++ rs_screener.py
@@ -14,15 +14,18 @@ start = dt.datetime(startyear,startmonth,startday)
 
 now = now=dt.datetime.now()
 
+stock=input("Enter a stock ticker symbol: ")
+
+dataFrame=pdr.get_data_yahoo(stock,start,now)
+
 newDf = pd.DataFrame()
 
 SPYIndex = pdr.get_data_yahoo("SPY",start,now)["Adj Close"]
 
+low_of_52week = min(dataFrame["Adj Close"][-260:]) #Finds minimum value of last 260 closing prices (260 trading days in 52 weeks)
+high_of_52week = max(dataFrame["Adj Close"][-260:]) #Finds maximum value of last 260 closing prices (260 trading days in 52 weeks)
+
 #Add for loop to check muptiple stocks
-
-stock=input("Enter a stock ticker symbol: ")
-
-dataFrame=pdr.get_data_yahoo(stock,start,now)
 
 dataFrame["RS"] = (dataFrame["Adj Close"]/SPYIndex)*100
 
