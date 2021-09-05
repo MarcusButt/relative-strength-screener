e6345415b63147038711b41bb7fac2b14c991266
MarcusButt <MarcusButt@users.noreply.github.com>
2021-09-05 18:06:35 -0230

    Added for Loop to Check Multiple Stocks and Add Results to new DF

--- rs_screener.py
+++ rs_screener.py
@@ -18,32 +18,29 @@ stocks=list(map(str,input("Enter up to five stock ticker symbols: ").split()))
 
 print(stocks) 
 
-dataFrame=pdr.get_data_yahoo(stocks,start,now)
-
-print(dataFrame)
-
 newDf = pd.DataFrame()
 
 SPYIndex = pdr.get_data_yahoo("SPY",start,now)
 
 print(SPYIndex)
 
-low_of_52week = min(dataFrame["Adj Close"][-260:]) #Finds minimum value of last 260 closing prices (260 trading days in 52 weeks)
-high_of_52week = max(dataFrame["Adj Close"][-260:]) #Finds maximum value of last 260 closing prices (260 trading days in 52 weeks)
+for stock in stocks:
+    dataFrame=pdr.get_data_yahoo(stock,start,now)
 
-#Add for loop to check muptiple stocks
+    print(dataFrame)
 
-#for x in stocks:
+    low_of_52week = min(dataFrame["Adj Close"][-260:]) #Finds minimum value of last 260 closing prices (260 trading days in 52 weeks)
+    high_of_52week = max(dataFrame["Adj Close"][-260:]) #Finds maximum value of last 260 closing prices (260 trading days in 52 weeks)
 
-dataFrame["RS"] = ((dataFrame["Adj Close"]/SPYIndex["Adj Close"])/(dataFrame["Adj Close"][-260]/SPYIndex["Adj Close"][-260]))*100
+    dataFrame["RS"] = (dataFrame["Adj Close"]/SPYIndex["Adj Close"])*100
 
-newDf[stocks] = dataFrame["RS"]
+    newDf["RS_"+stock] = dataFrame["RS"]
 
-print(newDf[stocks])
+    print(newDf)
 
-currentClose = dataFrame["Adj Close"][-1] #Access most recent close price from Yahoo finance database
+    currentClose = dataFrame["Adj Close"][-1] #Access most recent close price from Yahoo finance database
 
-if currentClose >= (0.8*high_of_52week): 
-    print("Near 52 Week High")
-else:
-    print("Out of Range")
+    if currentClose >= (0.8*high_of_52week): 
+        print("Near 52 Week High")
+    else:
+        print("Out of Range")
